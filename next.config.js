cbdff89cdc92a1b86af1ca630c96a51df76e5116
Mokut001 <mokutoton01@gmail.com>
2026-02-24 01:34:22 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.experiments = { asyncWebAssembly: true, topLevelAwait: true, layers: true };
-    return config;
-  },
-};
-module.exports = nextConfig;
